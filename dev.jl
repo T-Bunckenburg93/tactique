968f6dd3e945dd31660cfb265ee901469bd01601
ruthless-nz <51357492+ruthless-nz@users.noreply.github.com>
2024-06-26 19:28:48 +0100

    yea look dunno

--- dev.jl
+++ dev.jl
@@ -1,85 +1,10 @@
-include("main.jl")
+# include("main.jl")
+
 GLMakie.activate!(inline=false)
 
 BATTLEMAP = battleMap(1000,1000)
 
 
-
-# ok, so how can we generate an event from this?
-"""
-generates an event from a vision report that may be sent to the player. 
-"""
-function GenerateVisReport(u1,u2,mp,confidence,real=true)
-
-# possible information to include in the event:
-    EPos  = u2.position
-    EDest = u2.destination
-    ESpeed = u2.speed
-    ESupplies = u2.supplies
-    ECombatStrength = u2.combatStrength
-    Emorale = u2.morale
-    ESoliderCnt = u2.soliderCnt/100
-    EstaringSoliderCnt = u2.staringSoliderCnt
-    Elosses =  ESoliderCnt/EstaringSoliderCnt
-    EInfluenceRadius = u2.InfluenceRadius
-    Estealth = u2.stealth
-    Erecon = u2.reconissance
-    # Edoctrine = u2.doctrine
-    EType = u2.type
-    if !real
-    ESpeed = round(rand(Float64)*10)
-    # ESoliderCnt = round(rand(Float64)*1000)
-    EDest = Point2f(mp.position[1] + rand(1:10),mp.position[2] + rand(1:10))
-    end
-
-    d = Dict()
-    # want to be able to mix up fake and real reports
-    if rand(Float64) > 0.8
-        d["Enemy_Speed"] = ESpeed
-    end
-    if rand(Float64) > 0.8
-        d["Enemy_Dest"] = EDest
-    end
-
-    if confidence > 0.75
-        if rand(Float64) > 0.5
-            d["Enemy_Type"] = EType
-        end
-        if rand(Float64) > 0.8
-            d["Enemy_morale"] = Emorale
-        end
-        if rand(Float64) > 0.8
-            d["losses"] = Elosses
-        end
-
-        if rand(Float64) > 0.8
-            d["Enemy_Supplies"] = ESupplies
-        end
-        if rand(Float64) > 0.8
-            d["Enemy_CombatStrength"] = ECombatStrength
-        end
-    elseif confidence > 0.8
-        
-        if rand(Float64) > 0.9
-            d["Enemy_Pos"] = EPos
-        end
-        if rand(Float64) > 0.9
-            d["Enemy_Dest"] = EDest
-        end
-        if rand(Float64) > 0.9
-            d["EInfluenceRadius"] = EInfluenceRadius
-        end
-        if rand(Float64) > 0.9
-            d["Estealth"] = Estealth
-        end
-        if rand(Float64) > 0.9
-            d["Erecon"] = Erecon
-        end
-    end
-
-    event = Event("team1",1,"recon",confidence,u1.id,mp.position,d)
-    return event
-end
 
 
 
@@ -101,7 +26,7 @@ activeUnits = [
     ]
 graveyard = []
 
-plotVisions(activeUnits,3)
+plotVisions(activeUnits,1)
 
 
 # m = viz1[1]
@@ -113,42 +38,57 @@ EVENTS
 
 sort!(EVENTS,by = x -> x.reliability,rev = true)
 
-setMoveOrder!(recon1,Point2f(28,22))
+setMoveOrder!(recon1,Point2f(27,22))
+setMoveOrder!(recon2,Point2f(26,16))
 
 # First up, execute move orders
 for u in activeUnits
     MoveToPoint!(u)
 end
 
-# we check if there are any vision events
+# This gets the vision events, and the points where vision is attained.
+visArray, visProb = getVisionEvents(activeUnits)
+
+visArray
+visProb
+
+# now we we want look for vision events, inside areas of influence.
+# Doctrine will have some effect on this. 
+# For now, we will just look for vision events in the influence area of recon units.
+
+influenceVisArray = []
+influenceVisProb = []
+
 for i in activeUnits
     for j in activeUnits
         if i != j && i.team != j.team
-
-            viz = getVisionOverlap(i,j)
+            influenceOverlap = union(getInfluencePoints(i),getInfluencePoints(j))
 
             # get the real reports
-            for cI in viz
-                mp = BATTLEMAP.points[cI[1],cI[2]]
-                p = reconCheck(mp,i,j)
-                println(p)
+            for xy in influenceOverlap
+                mp = BATTLEMAP.points[xy[1],xy[2]]
+                p = reconInfluenceCheck(mp,i,j)
+
                 if p > 0.5
                     push!(EVENTS,GenerateVisReport(i,j,mp,p,true))
+                    push!(visArray,cI)
+                    push!(visProb,p)
                     # println("Unit 1 sees something at (REAL)", mapPt)
                 end
             end
-            # get the fake ones
-            for cI in setdiff(getVisionPoints(i),viz)
-                mp = BATTLEMAP.points[cI[1],cI[2]]
 
-                if rand(Float64) > 0.995 # this is the chance of spotting something that is not real. 
-                    push!(EVENTS,GenerateVisReport(i,j,mp,rand(0.5..0.65),false))
-                    # println("Unit 1 sees something at (FAKE)",mapPt)
-                end
-            end 
         end
     end
 end
+
+
+
+
+
+
+
+
+
 
 
 

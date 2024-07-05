70ea04633694ba25efd91a29b59ee2db412e411e
ruthless-nz <51357492+ruthless-nz@users.noreply.github.com>
2024-07-05 02:02:15 +0100

    lines! plotting was keeping artifacts. linesegments was needed to rm. Units now move around normally

---
+++ BattleMap.jl
@@ -0,0 +1,254 @@
+using GLMakie
+
+u1 = unit("t1u1",team = "team1",position = Point2f(1000,1000),angle = 0)
+u2 = unit("t1u1",team = "team1",position = Point2f(2000,2000),angle = 90)
+u3 = unit("t1u1",team = "team1",position = Point2f(3000,3000),angle = 180)
+u4 = unit("t1u1",team = "team1",position = Point2f(4000,4000),angle = 270)
+
+    activeUnits = [
+    u1,
+    # u2,
+    # u3,
+    # u4
+    ]
+    ActiveO =  Observable(activeUnits) 
+
+    graveyard = [
+    # u1,
+    # u2,
+    # u3,
+    # u4,
+    ]
+
+
+ActiveO[] = activeUnits
+
+# println("activeUnits: ",activeUnits)
+allU_sz = size(activeUnits,1)+size(graveyard,1)
+
+# Init Plots
+s = Figure(size = (10000, 10000))
+# Axis(s[1, 1], limits = ((-100, 100), (-100,100)))
+ax = Axis(s[1, 2], limits = ((-0, 10000), (-0,10000)), aspect = 1)
+# println("interaction: ",interactions(ax))
+deregister_interaction!(ax, :rectanglezoom)
+# deregister_interaction!(ax, :dragpan)
+hidespines!(ax)
+
+# ok, so what do we need to be able to plot?
+idx = Observable(1) # this is the index of the selected observable
+
+CurrentUnit = @lift getUnit($ActiveO,$idx)
+
+teamMap = Dict("team1" => "red", "team2" => "blue", "teamDev" => "pink")
+speed_vals = [20,50,80,100,200]
+
+
+ActiveO = Observable(activeUnits)
+
+
+
+# teamColours = Observable(fill("red",allU_sz))
+# teamUnitNames = Observable(fill("",allU_sz))
+# positions = Observable(fill(Point2f(-9999999999,-9999999999),allU_sz))
+# linePositions = Observable(fill((Point2f(-9999999999,-9999999999),Point2f(-9999999999,-9999999999)),allU_sz))
+
+teamColours = @lift [teamMap[i.team] for i in $ActiveO]
+teamUnitNames = @lift  [i.name for  i in $ActiveO]
+positions = @lift [i.position for i in $ActiveO]
+linePositions = @lift [(i.position,coalesce(i.destination,i.position)) for i in $ActiveO]
+boxO = @lift vec(stack([get_rectangle_points(i.position,i.width,i.depth,i.angle) for i in $ActiveO]))
+controlO = @lift vec(stack([controlArc(i.position, i.width, i.depth, i.angle,i.controlDistance,i.controlAngle) for i in $ActiveO ]))
+# controlO2 = @lift [controlArc(i.position, i.width, i.depth, i.angle,i.controlDistance,i.controlAngle) for i in $ActiveO ]
+boxO[]
+
+controlArc(activeUnits[1].position, activeUnits[1].width, activeUnits[1].depth, activeUnits[1].angle,activeUnits[1].controlDistance,activeUnits[1].controlAngle)
+linesegments!(controlO)
+# controlO2[]
+linesegments!(boxO)
+s
+# in order to stop length mismatch, we need to have the observables be a static length.
+# then we can update the values of the observables as needed.
+
+# on(ActiveO) do s
+
+#     controlO[] = getControlArc(ActiveO[])
+#     boxO[] = getSquare(ActiveO[])
+
+# end
+# notify(ActiveO) # init the above observables. 
+
+currentObjectSpeed_i = Observable{Any}(0.0)
+
+# Ok so I want to make a whole load of observables that are linked to each unit. 
+# this needs to come from all units. 
+
+
+linesegments!(linePositions,  color = teamColours)
+scatter!(positions, strokewidth = 3,  color =teamColours)
+
+text!(positions, text = teamUnitNames, color = teamColours, align = (:center, :top))
+s
+
+
+# When the current unit is updated, we need to update the menus
+# println("current unit: ",CurrentUnit[])
+on(idx) do s
+    println(typeof(s))
+    println("selected unit: $idx")
+
+    currentObjectSpeed_i = findfirst( ==(to_value(CurrentUnit).speed), speed_vals)
+    speed__M.i_selected = currentObjectSpeed_i
+
+end
+
+# Lets add menus
+TUN = filter(x -> x != "", to_value(teamUnitNames))
+# when I select a unit, I want to be able to change the attributes of that unit.
+selectUnit__M = Menu(s,
+    options = zip((TUN),collect(1:length(TUN))),
+    # selection = idx[]
+    )
+
+on(selectUnit__M.selection) do s
+    # println("selected unit: ",s)
+    idx[] = s
+end
+
+# now I want to add attributes of the selected unit that we can change.
+# when IDX changes, this needs to update.
+
+speed__M = Menu(s,
+    options = zip(string.("Speed ", speed_vals),collect(1:length(to_value(speed_vals)))),
+    )   
+
+on(speed__M.selection) do s
+println("speed change: Changing Unit ",CurrentUnit[].name," speed to : ",CurrentUnit[].speed)
+    cu = to_value(CurrentUnit)
+    cu.speed = speed_vals[s]*100
+    ActiveO[] = putUnit(activeUnits,idx[],cu)
+end
+
+
+s[1, 1] = vgrid!(
+    Label(s, "Unit:", width = nothing), 
+    selectUnit__M,
+    Label(s, "Attributes:", width = nothing),
+    speed__M
+    ;
+    tellheight = false, width = 200
+    )
+
+# button to run the simulation again. 
+
+RunButton = Button(s, label = "Run Simulation")
+on(RunButton.clicks, priority = 10) do s
+    tick()
+end
+
+
+# and add the units. 
+
+# Here are the keyboard interactions:
+
+
+# This selects a unit. 
+on(events(ax).mousebutton, priority = 2) do event
+
+    if event.button == Mouse.right && event.action == Mouse.press
+
+        plt, i = GLMakie.pick(s,10)
+        println(plt)
+        if plt isa Scatter{Tuple{Vector{Point{2, Float32}}}}
+
+            selectUnit__M.i_selected = i 
+        end
+    end
+
+    # println("positions: ",positions)
+
+    # if event.button == Mouse.right && event.action == Mouse.press && idx[] != 0
+    on(events(ax).mouseposition, priority = 3) do mp
+        mb = events(ax).mousebutton[]
+   
+        if to_value(events(ax).entered_window) == true && mb.button == Mouse.left && mb.action == Mouse.press
+            # get current unit and update destination
+            cu = to_value(CurrentUnit)
+            # println("current dest: ",cu.destination)
+            cu.destination = Point2f(mouseposition(ax))
+            CurrentUnit[] = cu
+
+            # push it back unto the active Unit, and update Observable
+            ActiveO[] = putUnit(activeUnits,idx[],cu)
+            notify(ActiveO)
+
+        end
+    end
+
+end
+
+display(s)
+# return to_value(ActiveO)
+
+
+
+
+function tick()
+    global ActiveO, activeUnits, graveyard
+
+    println(size(controlO[]))
+
+    # moof all units to their move orders.
+    for i = 1:4
+        println(i)
+
+        for u in activeUnits
+            # showFields(u,fields = [:position,:destination])
+            MoveToPoint!(u)
+        end
+
+        
+        # check to see if any units are overlapping, and apply engaged status.
+        # for i in activeUnits
+        #     for j in activeUnits
+        #         if i != j && i.team != j.team
+        #             if checkOverlap(i,j) in ["overlap","swallow","swallowed"]
+        #                 i.isEngaged = true
+        #             else 
+        #                 i.isEngaged = false
+        #             end
+        #             calculateCombat!(i,j)
+        #         end
+        #     end
+        # end
+        
+        # and apply them. 
+        # for i in activeUnits
+        #     applyCombat!(i)
+        #     # and see if any have died
+        #     if killCheck(i)
+        #         push!(graveyard,i)
+        #         filter!(x -> x != i, activeUnits)
+        #     end
+        # end
+
+        ActiveO[] = activeUnits
+        notify(ActiveO)
+
+        for i in ActiveO[] # this needs to be turned into a bunch of observables or create recipe
+            plotUnit!(ax,i)
+        end
+        # sleep(1)
+        display(s)
+    end
+    # activeUnits = plotInteractiveMap(activeUnits)
+end
+
+# t1u1 = unit("t1u1",team = "team1",position = Point2f(1000,1000))
+# tu2 = unit("t1u2",team = "team2",position = Point2f(5000,5000))
+
+# activeUnits = [t1u1,tu2]
+# graveyard = []
+
+# tick()
+s   
---
+++ Unit.jl
@@ -0,0 +1,344 @@
+using LinearAlgebra
+using GLMakie
+using GLMakie.GeometryBasics
+using Random
+using Distributions
+import Base: show
+
+"""
+# This is a simple implementation of a unit in a game.
+
+Name: Name of the Unit
+id: uniquely generated Int to identify the unit
+type: type of unit. inf/tank/recon
+team: team the unit belongs to
+positionX/positionY: xy coords of the unit
+InfluenceRadius: the radius of influence that the unit holds/contests
+soliderCnt: number of soldiers in the unit. this effects the size of the influence radius.
+combatStrength: the combat strength modifier of the unit
+morale: the morale modifier of the unit
+supplies: the supplies that the unit has to engage in combat.
+
+"""
+mutable struct Unit
+
+    name::String
+    id::Int
+    type::String
+    team::String
+    position::Point2f
+    destination::Point2f
+    angle::Number # in degrees
+    visionDistance::Number
+    controlDistance::Number
+    controlAngle::Number
+    spacing::Number
+    width::Number
+    depth::Number
+    staringSoliderCnt::Int # 0-99999
+    soliderCnt::Int # 0-99999
+    combatStrength::Number # 0,1,2
+    morale::Number # 0,1,2
+    supplies::Number
+    speed::Number  # in chunks/hour
+    stealth::Number # modifier of unit not being spotted
+    reconissance::Number # modifier of unit spotting other unitst
+    tactic::String # how the unit behaves in combat.
+end
+
+"""
+Constructor fucntion for the unit type. 
+"""
+function unit(
+        name::String; 
+        id::Int = rand(Int),
+        type="debugInfantry", 
+        team="teamDev", 
+        position = Point2f(0,0),
+        destination = missing,
+        angle = 0,
+        visionDistance = 2000,
+        controlDistance = 300,
+        controlAngle = 45,
+        spacing = 10,
+        depth = 200,
+        staringSoliderCnt = 300,
+        soliderCnt = 300,
+        combatStrength = 1,
+        morale = 1,
+        supplies = 1000,
+        speed = 100,
+        stealth = 1,
+        reconissance = 1,
+        tactic = "CONTROL",
+        )
+
+        if ismissing(destination)
+            destination = position
+        end
+
+        width = soliderCnt/3 * spacing
+
+        # construct the unit here
+    u = Unit(
+            name,
+            id,
+            type, 
+            team, 
+            position, 
+            destination,
+            angle,
+            visionDistance,
+            controlDistance,
+            controlAngle,
+            spacing,
+            width,            
+            depth,
+            staringSoliderCnt,
+            soliderCnt,
+            combatStrength,
+            morale,
+            supplies,
+            speed,
+            stealth,
+            reconissance,
+            tactic
+            )
+
+    return u
+end
+
+
+"""
+function that pulls out the values of a unit.
+    and throws them into a vector.
+"""
+function uVals(u::Unit)
+    v = []
+    for i in fieldnames(Unit)
+
+        push!(v, getfield(u,i))
+    end
+    return v
+end
+
+# First up. Lets plot this bad boi
+
+function get_rectangle_points(center, width, height, angle)
+
+    angle = deg2rad(angle)
+    # Center of the rectangle
+    x_c, y_c = center
+    
+    # Half dimensions
+    half_width = width / 2
+    half_height = height / 2
+    
+    # Define the corners relative to the center (no rotation)
+    corners = [
+        [ half_width,  half_height],
+        [-half_width,  half_height],
+        [-half_width, -half_height],
+        [ half_width, -half_height]
+    ]
+    
+    # Rotation matrix
+    cos_theta = cos(angle)
+    sin_theta = sin(angle)
+    rotation_matrix = [
+        cos_theta -sin_theta;
+        sin_theta  cos_theta
+    ]
+    
+    # Rotate and translate corners
+    rotated_corners = [rotation_matrix * corner for corner in corners]
+    translated_corners = [corner .+ [x_c, y_c] for corner in rotated_corners]
+    # fullBox = Point2f.(vcat(translated_corners,translated_corners[1]))
+    fullBox = Point2f.(translated_corners)
+
+    lineSegs = collect(zip(fullBox[1:end-1], fullBox[2:end]))
+
+    push!(lineSegs,(Point2f(translated_corners[1]),Point2f(translated_corners[3])))
+    push!(lineSegs,(Point2f(translated_corners[2]),Point2f(translated_corners[4])))
+    push!(lineSegs,(Point2f(translated_corners[1]),Point2f(translated_corners[4])))
+
+    
+    # return Point2f.(
+    #         vcat(translated_corners, 
+    #             [translated_corners[1]],
+    #             [NaN, NaN],
+    #             [translated_corners[3]],
+    #             [translated_corners[1]],
+    #             [NaN, NaN],
+    #             [translated_corners[2]],
+    #             [translated_corners[4]],
+    #             [NaN, NaN]
+    #             ))
+    return lineSegs
+end
+
+function controlArc(center, width, height, angleD,controlR,controlAngle)
+    
+    # firstup we get the control rectange directly in front, and shunt it fowards.
+    angle = deg2rad(angleD)
+    # Center of the rectangle
+    x_c, y_c = center
+    
+    # Half dimensions
+    half_width = width / 2
+    half_height = height / 2
+    
+    # Define the corners relative to the center (no rotation)
+    corners = [
+        [ half_width,  half_height + controlR],
+        [-half_width,  half_height + controlR],
+        [-half_width, half_height],
+        [ half_width, half_height]
+    ]
+    
+    # Rotation matrix
+    cos_theta = cos(angle)
+    sin_theta = sin(angle)
+    rotation_matrix = [
+        cos_theta -sin_theta;
+        sin_theta  cos_theta
+    ]
+    
+    # Rotate and translate corners
+    rotated_corners = [rotation_matrix * corner for corner in corners]
+    translated_corners = [corner .+ [x_c, y_c] for corner in rotated_corners]
+
+    # now we find the arcs either side and convert them into points. 
+
+    leftP = translated_corners[1]
+    rightP = translated_corners[2]
+    # controlR = controlR
+    num_points = 100
+    startA = angle + deg2rad(90 - controlAngle)
+    midA = angle + deg2rad(90)
+    endA = angle + deg2rad(90 + controlAngle)
+
+    lAngle = range(startA, midA, length=num_points)
+    rAngle = range(midA, endA, length=num_points)
+
+    lPoints = [leftP .+ controlR * Point2f0(cos(θ), sin(θ)) for θ in lAngle]
+    rPoints = [rightP .+ controlR * Point2f0(cos(θ), sin(θ)) for θ in rAngle]
+
+    arc_points = Point2f.(vcat(lPoints,rPoints))
+    arcSegments = collect(zip(arc_points[1:end-1], arc_points[2:end]))
+
+    # return Point2f.(arcSegments)
+    return arcSegments
+
+end
+
+# points = [1, 2, 3, 4]
+# segments = collect(zip(points[1:end-1], points[2:end]))
+
+function plotUnit!(axis,unit::Unit, unitColor = :red)
+    # get the boxes of the units.
+    rectangle_points = get_rectangle_points(unit.position, unit.width, unit.depth, unit.angle)
+    control_Arc = controlArc(unit.position, unit.width, unit.depth, unit.angle, unit.controlDistance, unit.controlAngle)
+
+    lines!(axis,rectangle_points, color = unitColor)
+    lines!(axis,control_Arc, color = (:blue,0.5))
+
+end
+
+# lets add some movement stuff to the unit. 
+
+"""
+# basic movement function that sets the position that the unit ends up at. 
+works with either a point or x,y coords.
+"""
+function teleportUnit!(unit::Unit, p::Point2f)
+    unit.position = p
+    return unit
+end
+function teleportUnit!(unit::Unit, x::Number, y::Number)
+    p = Point2f(x,y)
+    teleportUnit!(unit,p)
+    return unit
+end
+
+
+"""
+This takes a unit and a point and sets the unit to move to that point.
+"""
+function  setMoveOrder!(unit::Unit,position::Point2f; speed=missing)
+    unit.destination = position
+    if !ismissing(speed)
+        unit.speed = speed
+    end
+    return unit
+end
+function setMoveOrder!(unit::Unit, x::Number, y::Number; speed=missing)
+    p = Point2f(x,y)
+    setMoveOrder!(unit,p,speed=speed)
+    return 
+end
+function setMoveOrder!(unit::Observable, x::Number, y::Number; speed=missing)
+    u = to_value(unit)
+    p = Point2f(x,y)
+    setMoveOrder!(u,p,speed=speed)
+    unit[] = u
+    return 
+end
+
+# and here is a function to execute these move orders
+
+"""
+This moves the unit towards the destination by the amount set in unit.speed. 
+"""
+function MoveToPoint!(unit::Unit)
+
+    if ismissing(unit.destination)
+        return
+    end
+
+    speed = unit.speed
+
+    # get the distance between the two points
+    distance = sqrt((unit.position[1] - unit.destination[1])^2 + (unit.position[2] - unit.destination[2])^2)
+    if distance <  speed
+        teleportUnit!(unit, unit.destination)
+        return
+    else
+
+        # move it towards the point. first get the unit vector from the unit to the desired point
+        v = [unit.destination[1] - unit.position[1], unit.destination[2] - unit.position[2]]
+        vNorm = v / norm(v)
+        # move the unit in that direction
+        teleportUnit!(unit, unit.position[1] + vNorm[1] * speed, unit.position[2] + vNorm[2] * speed)
+        ### Will need to add in supply consuption
+        # and remove supplies equal to 1 + 1/10th of the distance moved.
+        # unit.supplies = max(unit.supplies - 1 - distance/10,0)
+    end
+end
+function MoveToPoint!(unit::Observable)
+    u = to_value(unit)
+    MoveToPoint!(u)
+    unit[] = u
+end
+
+
+"""
+Utility function for pulling out the ith unit from a list of units.
+"""
+function getUnit(activeUnits,i)
+    return activeUnits[i]
+end
+"""
+Utility function for putting a unit back into a list of units at point n
+    """
+function putUnit(activeUnits,i,u)
+    activeUnits[i] = u
+    return activeUnits
+end
+
+
+u1 = unit("unit1", position = Point2f(5000,5000),spacing = 5, depth = 100, angle = 45, controlDistance = 350)
+f = Figure()
+ax = Axis(f[1, 1], aspect  =DataAspect(), limits = ((0,10000),(0,10000)))
+plotUnit!(ax,u1)
+f
